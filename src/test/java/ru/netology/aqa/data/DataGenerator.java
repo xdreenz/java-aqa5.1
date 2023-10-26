86c65e08cde79b62da734692372903c0f59c2a66
Akaten test <gxconv@gmail.com>
2023-10-26 15:07:46 +0300

    Initial commit

---
+++ src/test/java/ru/netology/aqa/data/DataGenerator.java
@@ -0,0 +1,46 @@
+package ru.netology.aqa.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {}
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.address().cityName();
+    }
+
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().cellPhone();
+    }
+
+    public static class Registration {
+        private Registration() {}
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/aqa/test/CardDeliveryFormTest.java
@@ -0,0 +1,47 @@
+package ru.netology.aqa.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.aqa.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryFormTest {
+
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSubmitSuccessfully() throws InterruptedException {
+        var userInfo = DataGenerator.Registration.generateUser("ru");
+        String date1 = DataGenerator.generateDate(10);
+        String date2 = DataGenerator.generateDate(15);
+        $("[data-test-id = city] input").setValue(userInfo.getCity());
+        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id = date] input").setValue(date1);
+        $("[data-test-id = name] input").setValue(userInfo.getName());
+        $("[data-test-id = phone] input").setValue(userInfo.getPhone());
+        $("[data-test-id = agreement]").click();
+//        Thread.sleep(10000);
+        $(byText("Запланировать")).click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + date1));
+
+        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id = date] input").setValue(date2);
+        $(byText("Запланировать")).click();
+        $(byText("Перепланировать")).click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + date2));
+
+    }
+}
